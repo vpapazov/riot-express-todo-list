ce8d117c3602be8a9a513d5cdbe1790d0147510b
Valeriia Papazova <papazovaleria@gmail.com>
2018-08-29 16:04:05 -0700

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -86,9 +86,9 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.223.122.214',
+      host: '18.219.156.206',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: '/home/desktop/riot-express-todo-list/hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
